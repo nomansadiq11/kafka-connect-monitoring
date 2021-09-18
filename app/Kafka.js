0000b50045f257406ea5613135e3f6fa7a6ee452
nomansadiq11 <nomi.cs786@gmail.com>
2021-09-19 00:13:38 +0400

    connector api added

---
+++ app/Kafka.js
@@ -0,0 +1,83 @@
+
+
+var ServiceURL = "http://cp-kafka-connect.sandbox.grid2.maf.ae/";
+
+var app = angular.module("KafkaApp", ['ngCookies']);
+
+
+app.service("kafkaService", function ($http) {
+
+
+    this.PostToService = function (param, MethodName) {
+        var response = $http({
+            method: "post",
+            url: ServiceURL +  MethodName,
+            data: JSON.stringify(param),
+            dataType: "json"
+        });
+        return response;
+    }
+
+    this.PostToServiceWithoutParam = function (MethodName) {
+        var response = $http({
+            method: "post",
+            url: ServiceURL +  MethodName,
+            data: '',
+            headers: {'Access-Control-Allow-Origin': '*'},
+            dataType: "json"
+        });
+        return response;
+    }
+
+
+
+});
+
+
+
+
+
+
+
+
+app.controller("kafkaController", ['$scope', '$cookies', '$cookieStore', '$window' ,'kafkaService', function($scope, $cookies, $cookieStore, $window, kafkaService) {
+
+
+    $scope.fn_GetConnectorsList = function () {
+
+        debugger;
+
+        
+                
+        var param =            
+                {
+                    "query": $scope.query
+                }; 
+
+        $('#loader').show(); 
+
+
+        var ResponseRegistration = kafkaService.PostToServiceWithoutParam("connectors");
+        ResponseRegistration.then(function (msg) {
+            
+            $scope.connectors = msg.data;
+            
+
+            $('#loader').hide(); 
+
+            
+        }, function (msg) {
+
+            console.log('Error: AppInsightsService');
+            $('#loader').hide(); 
+        });
+    }
+
+
+   
+
+    
+
+    
+
+}]);
